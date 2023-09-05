765e1d32aab10750e20596db15f12ba8095917d2
Weiry <gonweiry@gmail.com>
2023-09-05 20:29:29 -0300

    Initial Commit

---
+++ app/lib/login_page.dart
@@ -0,0 +1,251 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_dispositivos_moveis/recovery_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        /* brightness: Brightness.light */
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: ListView(
+          /* mainAxisAlignment: MainAxisAlignment.spaceBetween, */
+          children: <Widget>[
+            const Center(
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: Color.fromRGBO(13, 17, 23, 1)),
+              ),
+            ),
+            Container(
+              height: size.height * 0.35,
+              decoration: const BoxDecoration(
+                  image:
+                      DecorationImage(image: AssetImage("assets/login.png"))),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              width: size.width * 0.7,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border:
+                      Border.all(color: const Color.fromRGBO(12, 17, 23, 1)),
+                  borderRadius: BorderRadius.circular(30)),
+              child: const TextField(
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.email,
+                      color: Color.fromRGBO(59, 81, 108, 1),
+                    ),
+                    hintText: "Email",
+                    border: InputBorder.none),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              width: size.width * 0.7,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border:
+                      Border.all(color: const Color.fromRGBO(12, 17, 23, 1)),
+                  borderRadius: BorderRadius.circular(30)),
+              child: TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                    icon: const Icon(
+                      Icons.lock,
+                      color: Color.fromRGBO(59, 81, 108, 1),
+                    ),
+                    suffixIcon: togglePassword(),
+                    hintText: "Password",
+                    border: InputBorder.none),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 120,
+              ),
+              child: TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RecoveryPage()));
+                },
+                child: const Text(
+                  'ESQUECEU A SENHA?',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    color: Color.fromRGBO(13, 17, 23, 1.0),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              width: size.width * 0.7,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 26, 43, 64),
+                  borderRadius: BorderRadius.circular(30)),
+              child: MaterialButton(
+                onPressed: () {},
+                child: const Text(
+                  "ENTRAR",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: <Color>[
+                            Color.fromRGBO(255, 255, 255, 0.102),
+                            Color.fromARGB(255, 43, 32, 72),
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(1.0, 1.0),
+                          stops: <double>[0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                    ),
+                    width: 100.0,
+                    height: 1.0,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                    child: Text(
+                      'Ou entre com',
+                      style: TextStyle(
+                          color: Color.fromRGBO(13, 17, 23, 1),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'WorkSansMedium'),
+                    ),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: <Color>[
+                            Color.fromARGB(255, 43, 32, 72),
+                            Color.fromRGBO(255, 255, 255, 0.102),
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(1.0, 1.0),
+                          stops: <double>[0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                    ),
+                    width: 100.0,
+                    height: 1.0,
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: ElevatedButton.icon(
+                      icon: const Icon(
+                        Icons.facebook,
+                        color: Color.fromRGBO(53, 120, 229, 1),
+                      ),
+                      label: const Text(
+                        "FACEBOOK",
+                        style: TextStyle(
+                          color: Color.fromRGBO(12, 17, 23, 1),
+                        ),
+                      ),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            side: const BorderSide(
+                                color: Color.fromRGBO(13, 17, 23, 1)),
+                            borderRadius: BorderRadius.circular(50)),
+                      ),
+                    )),
+                const SizedBox(width: 15),
+                Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: ElevatedButton.icon(
+                      icon: Image.network(
+                        "https://img.icons8.com/fluency/48/google-logo.png",
+                        width: 20,
+                      ),
+                      label: const Text(
+                        "GOOGLE",
+                        style: TextStyle(
+                          color: Color.fromRGBO(12, 17, 23, 1),
+                        ),
+                      ),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            side: const BorderSide(
+                                color: Color.fromRGBO(13, 17, 23, 1)),
+                            borderRadius: BorderRadius.circular(50)),
+                      ),
+                    )),
+              ],
+            ),
+            const SizedBox(height: 30)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget togglePassword() {
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          _obscureText = !_obscureText;
+        });
+      },
+      icon: _obscureText
+          ? const Icon(Icons.visibility)
+          : const Icon(Icons.visibility_off),
+      color: const Color.fromRGBO(82, 115, 156, 1),
+    );
+  }
+}
---
+++ app/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_dispositivos_moveis/login_page.dart';
+import 'package:projeto_dispositivos_moveis/signup_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Column(
+                children: <Widget>[
+                  Text(
+                    "Bem-vindo",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(13, 17, 23, 1)),
+                  )
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 2,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/cart_welcome.png"))),
+              ),
+              Column(
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                            color: Color.fromRGBO(13, 17, 23, 1)),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: const Text(
+                      "Login",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUpPage()));
+                    },
+                    color: const Color.fromARGB(255, 26, 43, 64),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    child: const Text(
+                      "Cadastro",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
